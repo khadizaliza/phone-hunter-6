e61e3449e0c945fa43278a0d1de307a204ece6d8
khadizaliza <khadizaakter19836@gmail.com>
2022-03-02 22:02:38 +0600

    card-section

--- js/phone.js
+++ js/phone.js
@@ -4,16 +4,19 @@ const searchButton = () =>{
     const inputValue = document.getElementById("input-value");
     const searchText = inputValue.value;
     inputValue.value = '';
+    // fetch
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     console.log(url);
     fetch(url)
     .then(res => res.json())
-    .then(data => showPhoneDetails(data));
+    .then(data => showPhoneDetails(data.data));
   // console.log(inputValue);
     
     
 }
+//card-blog
 const showPhoneDetails = phones => {
+    console.log(phones)
     const searchResult = document.getElementById('search-result');
      phones.forEach(phone => {
          console.log(phones);
@@ -21,10 +24,10 @@ const showPhoneDetails = phones => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${phone.image}" class="card-img-top w-25" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="text-info">Name:${phone.phone_name}</h5>
+          <button class="btn btn-info text-white">Detail</button>
         </div>
       </div>
       `;
